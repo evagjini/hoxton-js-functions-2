1a580e67b6835ef8ee1af12f94e1b6db6e6ae902
Fevronia Gjini <fevronigjini1@gmail.com>
2022-06-21 00:31:24 +0200

    final

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 
 
-//  - Say hi to those users in the console - but only 1 greeting every 2 seconds.
-//  - Prompt the user for a user ID.
-//  Using the value, find all the todos that are Incomplete and console.log them.
+
+
+
 
 
 //  - Prompt the user for a letter
@@ -13,10 +13,29 @@ let userPickALetter = String(prompt("Choose a Letter!"));
 
 //  - Find all the users who's name contains the letter 
 
-const findUsers = users.filter(function (user)
+let foundUsers = users.filter(function (user)
 { return user.name.includes(userPickALetter)
 
-}
+})
+// console.log(foundUsers)
+
+
+//  - Say hi to those users in the console - but only 1 greeting every 2 seconds.
+
+setTimeout (function ()
+{console.log(`Hi ${foundUsers}`), 2000}
 )
-console.log(`findUsers:`, )
-let userIndex = 1
+
+
+
+//  - Prompt the user for a user ID.
+
+
+
+let userId = Number(prompt("Enter your userid"));
+
+
+//  Using the value, find all the todos that are Incomplete and console.log them.
+let findAllTodo = todos.filter(
+(todo) => todo.userId === userId && todo.completed === false) 
+console.log(findAllTodo)
